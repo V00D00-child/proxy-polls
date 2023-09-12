db563e9fb6f36e4e0867d559a56313c7aafb6759
Idris Bowman <34751375+V00D00-child@users.noreply.github.com>
2023-09-12 15:46:13 -0400

    Initial commit

---
+++ scripts/deploy.js
@@ -0,0 +1,53 @@
+
+const { network } = require("hardhat");
+const path = require("path");
+
+async function main() {
+  if (network.config.chainId !== 1337 && network.config.chainId !== 31337) {
+    console.log('NOT deploying contracts. use pre-deployed contracts')
+    process.exit(1)
+  }
+
+  const [deployer] = await ethers.getSigners();
+  console.log(
+    "Deploying the contracts with the account:",
+    await deployer.getAddress()
+  );
+
+  const Token = await ethers.getContractFactory("Token");
+  const token = await Token.deploy();
+  await token.deployed();
+
+  console.log("Token address:", token.address);
+
+  // We also save the contract's artifacts and address in the frontend directory
+  saveFrontendFiles(token);
+}
+
+function saveFrontendFiles(token) {
+  const fs = require("fs");
+  const contractsDir = path.join(__dirname, "..", "frontend", "src", "contracts", ``);
+
+  if (!fs.existsSync(contractsDir)) {
+    fs.mkdirSync(contractsDir);
+  }
+
+  fs.writeFileSync(
+    path.join(contractsDir, "contract-address.json"),
+    JSON.stringify({ Token: token.address }, undefined, 2)
+  );
+
+  const TokenArtifact = artifacts.readArtifactSync("Token");
+
+  fs.writeFileSync(
+    path.join(contractsDir, "Token.json"),
+    JSON.stringify(TokenArtifact, null, 2)
+  );
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
